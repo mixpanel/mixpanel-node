d4af0333c745f03c33f91ea0aa66794f450e45da
Carl Sverre <carl@carlsverre.com>
2012-09-05 17:45:31 -0700

    people.increment unit tests, debug check, init() config tests

--- test/people.js
+++ test/people.js
@@ -55,5 +55,77 @@ exports.people = {
 
             test.done();
         }
-    }
+    },
+
+    increment: {
+        "calls send_request with correct endpoint and data": function(test) {
+            var expected_data = {
+                    $add: { key1: 1 },
+                    $token: this.token,
+                    $distinct_id: this.distinct_id
+                };
+
+            this.mixpanel.people.increment(this.distinct_id, 'key1');
+
+            test.ok(
+                this.mixpanel.send_request.calledWithMatch(this.endpoint, expected_data),
+                "people.increment didn't call send_request with correct arguments"
+            );
+
+            test.done();
+        },
+
+        "supports incrementing key by value": function(test) {
+            var expected_data = {
+                    $add: { key1: 2 },
+                    $token: this.token,
+                    $distinct_id: this.distinct_id
+                };
+
+            this.mixpanel.people.increment(this.distinct_id, "key1", 2);
+
+            test.ok(
+                this.mixpanel.send_request.calledWithMatch(this.endpoint, expected_data),
+                "people.increment didn't call send_request with correct arguments"
+            );
+
+            test.done();
+        },
+
+        "supports incrementing multiple keys": function(test) {
+            var prop = { key1: 5, key2: -3 },
+                expected_data = {
+                    $add: prop,
+                    $token: this.token,
+                    $distinct_id: this.distinct_id
+                };
+
+            this.mixpanel.people.increment(this.distinct_id, prop);
+
+            test.ok(
+                this.mixpanel.send_request.calledWithMatch(this.endpoint, expected_data),
+                "people.increment didn't call send_request with correct arguments"
+            );
+
+            test.done();
+        },
+
+        "ignores invalid values": function(test) {
+            var prop = { key1: "bad", key2: 3, key3: undefined, key4: "5", key5: new Date(), key6: function(){} },
+                expected_data = {
+                    $add: { key2: 3, key4: '5' },
+                    $token: this.token,
+                    $distinct_id: this.distinct_id
+                };
+
+            this.mixpanel.people.increment(this.distinct_id, prop);
+
+            test.ok(
+                this.mixpanel.send_request.calledWithMatch(this.endpoint, expected_data),
+                "people.increment didn't call send_request with correct arguments"
+            );
+
+            test.done();
+        }
+    },
 };
