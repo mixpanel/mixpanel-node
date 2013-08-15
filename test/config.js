3f1a31a5a3ce07081d8324199be770dea5db912e
William Bert <william@spanishdict.com>
2013-08-15 16:10:57 -0400

    Fix test.

--- test/config.js
+++ test/config.js
@@ -8,7 +8,7 @@ exports.config = {
 
     "is set to correct defaults": function(test) {
         test.deepEqual(this.mixpanel.config,
-                       { test: false, debug: false },
+                       { test: false, debug: false, verbose: false },
                        "default config is incorrect");
         test.done();
     },
